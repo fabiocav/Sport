53af922dc940684494d241bd1dec4ee699cf73fc
rob-derosa <rob.derosa@xamarin.com>
2015-03-13 15:51:49 -0700

    Initial commit

---
+++ XSTTLA.Shared/App.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XSTTLA.Shared
+{
+    public class App : Application
+    {
+        public App()
+        {
+            MainPage = new ContentPage {
+                Content = new Label {
+                    Text = "Hello, Forms !",
+                    VerticalOptions = LayoutOptions.CenterAndExpand,
+                    HorizontalOptions = LayoutOptions.CenterAndExpand,
+                }	
+            }; 
+        }
+    }
+}
