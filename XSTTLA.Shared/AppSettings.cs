bf9b1512891c85a94ffd7c13e1d69e075852ade5
Rob DeRosa <rob.derosa@xamarin.com>
2015-03-14 14:44:52 -0700

    App settings updates

---
+++ XSTTLA.Shared/AppSettings.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Refractored.Xam.Settings.Abstractions;
+using Refractored.Xam.Settings;
+
+namespace XSTTLA.Shared
+{
+	public class AppSettings
+	{
+		private static ISettings Instance
+		{
+			get
+			{
+				return CrossSettings.Current;
+			}
+		}
+
+		const string _authTokenKey = "auth_token";
+
+		public static string AuthToken
+		{
+			get
+			{
+				return Instance.GetValueOrDefault<string>(_authTokenKey);
+			}
+			set
+			{
+				Instance.AddOrUpdateValue(_authTokenKey, value);
+			}
+		}
+
+		const string _authUserIdKey = "user_id";
+
+		public static string AuthUserID
+		{
+			get
+			{
+				return Instance.GetValueOrDefault<string>(_authUserIdKey);
+			}
+			set
+			{
+				Instance.AddOrUpdateValue(_authUserIdKey, value);
+			}
+		}
+
+		public static UserProfile AuthUserProfile
+		{
+			get;
+			set;
+		}
+	}
+}
