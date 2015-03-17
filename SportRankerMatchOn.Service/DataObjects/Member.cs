5e5367c1ed7543daa3bc4c5639b1b83eccd2adf9
Rob DeRosa <rob.derosa@xamarin.com>
2015-03-16 20:24:47 -0700

    Added Azure service

---
+++ SportRankerMatchOn.Service/DataObjects/Member.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.WindowsAzure.Mobile.Service;
+
+namespace SportRankerMatchOn.Service.DataObjects
+{
+	public class Member : EntityData
+	{
+		public string Text
+		{
+			get;
+			set;
+		}
+
+		public bool Complete
+		{
+			get;
+			set;
+		}
+	}
+}
---
+++ SportRankerMatchOn.Service/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SportRankerMatchOn.Service.WebApiApplication" Language="C#" %>
