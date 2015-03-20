a9b4ab3c6d78903f7552a6fb13902ebd5aeb42d7
Rob DeRosa <rob.derosa@xamarin.com>
2015-03-20 15:59:23 -0700

    Azure integration server changes

--- SportRankerMatchOn.Service/Controllers/LeagueController.cs
+++ SportRankerMatchOn.Service/Controllers/LeagueController.cs
@@ -6,6 +6,8 @@ using System.Web.Http.OData;
 using Microsoft.WindowsAzure.Mobile.Service;
 using SportRankerMatchOn.Shared;
 using SportRankerMatchOn.Service.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace SportRankerMatchOn.Service.Controllers
 {
@@ -21,13 +23,16 @@ namespace SportRankerMatchOn.Service.Controllers
         // GET tables/League
         public IQueryable<League> GetAllLeagues()
         {
-            return Query();
+			return Query();
         }
 
         // GET tables/League/48D68C86-6EA6-4C25-AA33-223FC9A27959
         public SingleResult<League> GetLeague(string id)
         {
-            return Lookup(id);
+            var result = Lookup(id);
+			var league = result.Queryable.FirstOrDefault();
+			league.LoadMemberIds();
+			return result;
         }
 
         // PATCH tables/League/48D68C86-6EA6-4C25-AA33-223FC9A27959
@@ -39,6 +44,7 @@ namespace SportRankerMatchOn.Service.Controllers
         // POST tables/League
         public async Task<IHttpActionResult> PostLeague(League item)
         {
+			item.MemberIdsString = JsonConvert.SerializeObject(item.MemberIds);
             League current = await InsertAsync(item);
             return CreatedAtRoute("Tables", new { id = current.Id }, current);
         }
---
+++ SportRankerMatchOn.Service/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using SportRankerMatchOn.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportRankerMatchOn.Service
+{
+	public static class Extensions
+	{
+		public static void LoadMemberIds(this League league)
+		{
+			if(league != null)
+			{
+				if(!string.IsNullOrWhiteSpace(league.MemberIdsString))
+				{
+					league.MemberIds = JsonConvert.DeserializeObject<List<string>>(league.MemberIdsString);
+				}
+			}
+		}
+	}
+}
