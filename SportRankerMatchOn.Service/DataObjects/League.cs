79722ee7f3ed6a79943cacdc81d7bc706c40e4bc
Rob DeRosa <rob.derosa@xamarin.com>
2015-03-20 08:49:17 -0700

    Server side changes

--- SportRankerMatchOn.Service/DataObjects/League.cs
+++ SportRankerMatchOn.Service/DataObjects/League.cs
@@ -1,8 +1,44 @@
 ﻿using Microsoft.WindowsAzure.Mobile.Service;
+using System.Collections.Generic;
 
 namespace SportRankerMatchOn.Shared
 {
 	public partial class League : EntityData
 	{
+		public string Name
+		{
+			get;
+			set;
+		}
+
+		public int Season
+		{
+			get;
+			set;
+		}
+
+		public string Sport
+		{
+			get;
+			set;
+		}
+
+		public bool IsAcceptingMembers
+		{
+			get;
+			set;
+		}
+
+		public List<string> MemberIds
+		{
+			get;
+			set;
+		}
+
+		public bool IsEnabled
+		{
+			get;
+			set;
+		}
 	}
 }
