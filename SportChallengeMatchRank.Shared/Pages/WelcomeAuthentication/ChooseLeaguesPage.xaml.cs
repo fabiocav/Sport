9d4c8bc8a3f0915f48d3b54d8491822e4d9a7c5f
rob-derosa <rob.derosa@xamarin.com>
2015-04-26 23:21:50 -0700

    And one more

---
+++ SportChallengeMatchRank.Shared/Pages/WelcomeAuthentication/ChooseLeaguesPage.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SportChallengeMatchRank.Shared
+{
+	public partial class ChooseLeaguesPage : ChooseLeaguesPageXaml
+	{
+		public Action OnSave
+		{
+			get;
+			set;
+		}
+
+		public ChooseLeaguesPage()
+		{
+			Initialize();
+		}
+
+		protected async override void Initialize()
+		{
+			InitializeComponent();
+			Title = "Join a League";
+
+			MessagingCenter.Subscribe<App>(this, "RegisteredForRemoteNotifications", async(app) =>
+			{
+				btnPush.Text = "Thanks! We'll be in touch.";
+				await Task.Delay(1000);
+				await btnPush.LayoutTo(new Rectangle(Content.Width, btnPush.Bounds.Y, btnPush.Bounds.Width, btnPush.Height), 350, Easing.SinIn);
+			});
+
+			btnPush.Clicked += async(sender, e) =>
+			{
+				var push = DependencyService.Get<IPushNotifications>();
+
+				#if !DEBUG
+				if(push.IsRegistered)
+					return;
+				#endif
+
+				var task = push.RegisterForPushNotifications();
+				await ViewModel.RunSafe(task);
+			};
+
+			btnContinue.Clicked += (sender, e) =>
+			{
+				App.Current.MainPage = new MasterDetailPage();
+			};
+
+			list.ItemSelected += async(sender, e) =>
+			{
+				if(list.SelectedItem == null)
+					return;
+				
+				var league = list.SelectedItem as League;
+				list.SelectedItem = null;
+
+				if(league.Id == null)
+					return;
+
+				var detailsPage = new LeagueDetailsPage(league);
+				var navPage = new NavigationPage(detailsPage);
+				var cancel = new ToolbarItem {
+					Text = "Cancel",
+				};
+
+				detailsPage.OnJoinedLeague = (l) =>
+				{
+					ViewModel.LocalRefresh();
+					Navigation.PopModalAsync();
+				};
+					
+				cancel.Clicked += (sender2, e2) =>
+				{
+					Navigation.PopModalAsync();
+				};
+
+				navPage.ToolbarItems.Add(cancel);
+				await Navigation.PushModalAsync(navPage);	
+			};
+
+			await ViewModel.GetAvailableLeagues();
+		}
+
+		protected async override void OnLoaded()
+		{
+			base.OnLoaded();
+			label1.ScaleTo(1, 500, Easing.SinIn);
+
+			await Task.Delay(250);
+			leaguesStack.ScaleTo(1, 500, Easing.SinIn);
+
+			await Task.Delay(250);
+			buttonStack.ScaleTo(1, 500, Easing.SinIn);
+		}
+	}
+
+	public partial class ChooseLeaguesPageXaml : BaseContentPage<AvailableLeaguesViewModel>
+	{
+	}
+}
