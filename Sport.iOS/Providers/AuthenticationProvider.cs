63d5c9b88ff7db01113185fa2bc66153d6ef2100
Rob DeRosa <rob.derosa@xamarin.com>
2015-07-23 08:17:16 -0700

    Minor updates

--- Sport.iOS/Providers/AuthenticationProvider.cs
+++ Sport.iOS/Providers/AuthenticationProvider.cs
@@ -17,7 +17,7 @@ namespace Sport.iOS
 			{
 				var tcs = new TaskCompletionSource<Tuple<string, string>>();
 
-				var auth = new OAuth2Authenticator(Constants.GoogleApiClientId, Constants.GoogleClientSecret, Constants.GoogleOAuthScope, new Uri(Constants.GoogleOAuthAuthUrl), new Uri(Constants.GoogleOAthRedirectUrl), new Uri(Constants.GoogleOAuthTokenUrl));
+				var auth = new OAuth2Authenticator(Keys.GoogleApiClientId, Keys.GoogleClientSecret, Keys.GoogleOAuthScope, new Uri(Keys.GoogleOAuthAuthUrl), new Uri(Keys.GoogleOAthRedirectUrl), new Uri(Keys.GoogleOAuthTokenUrl));
 				auth.AllowCancel = true;
 				auth.ShowUIErrors = false;
 				auth.ClearCookiesBeforeLogin = false;
