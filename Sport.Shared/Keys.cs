e0eee4184b71393ca5f3609c8a95db3cd034dcb3
Rob DeRosa <rob.derosa@xamarin.com>
2015-07-23 00:33:12 -0700

    Minor updates

---
+++ Sport.Shared/Keys.cs
@@ -0,0 +1,22 @@
+﻿namespace Sport.Shared
+{
+	public class Keys
+	{
+		public static readonly string AzureDomain = "";
+		public static readonly string AzureClientId = "";
+
+		public static readonly string InsightsApiKey = "";
+
+		//Google Native Application
+		public static readonly string GoogleApiClientId = "";
+		public static readonly string GoogleClientSecret = "";
+		public static readonly string GoogleOAuthScope = "";
+		public static readonly string GoogleOAthRedirectUrl = "http://localhost";
+		public static readonly string GoogleOAuthAuthUrl = "";
+		public static readonly string GoogleOAuthTokenUrl = "";
+
+		public static readonly string FlikrApiKey = "";
+
+		public static readonly string SourceCodeUrl = "";
+	}
+}
