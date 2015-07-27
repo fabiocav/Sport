288eef88618e0c1ac1c635b8451162f855e0977d
rob-derosa <rob.derosa@xamarin.com>
2015-07-26 23:56:26 -0700

    Minor UI updates

--- Sport.Shared/Keys.cs
+++ Sport.Shared/Keys.cs
@@ -2,18 +2,18 @@
 {
 	public class Keys
 	{
-		public static readonly string AzureDomain = "http://sportchallengematchrank.azure-mobile.net/";
-		public static readonly string SourceCodeUrl = "https://github.com/rob-derosa/Sport";
-		public static readonly string AzureClientId = "FSKOHcURfLmRDDKzUdZBuOxpmAlQyc62";
-		public static readonly string InsightsApiKey = "23a3e3502a5258fbc6bf97bd85341af8b4a0b69b";
-		public static readonly string FlikrApiKey = "22a18bbd0b4f7c86975c81c8f3cb7719";
+		public static readonly string AzureDomain = "";
+		public static readonly string SourceCodeUrl = "";
+		public static readonly string AzureClientId = "";
+		public static readonly string InsightsApiKey = "";
+		public static readonly string FlikrApiKey = "";
 
 
-		public static readonly string GoogleApiClientId = "236481934978-balamfh77inmje2nfeq3bphdg3udt03t.apps.googleusercontent.com";
-		public static readonly string GoogleClientSecret = "wRYG9TlQZLXnlpUbu-0X_BwF";
-		public static readonly string GoogleOAuthScope = "profile email https://www.googleapis.com/auth/calendar";
+		public static readonly string GoogleApiClientId = "";
+		public static readonly string GoogleClientSecret = "";
+		public static readonly string GoogleOAuthScope = "profile email";
 		public static readonly string GoogleOAthRedirectUrl = "http://localhost";
-		public static readonly string GoogleOAuthAuthUrl = "https://accounts.google.com/o/oauth2/auth";
-		public static readonly string GoogleOAuthTokenUrl = "https://accounts.google.com/o/oauth2/token";
+		public static readonly string GoogleOAuthAuthUrl = "";
+		public static readonly string GoogleOAuthTokenUrl = "";
 	}
 }
