5fb74056866e67ba81541ae64e24f081496fc48c
rob-derosa <rob.derosa@xamarin.com>
2015-07-26 23:54:12 -0700

    Minor UI updates

--- Sport.Shared/Keys.cs
+++ Sport.Shared/Keys.cs
@@ -8,6 +8,7 @@
 		public static readonly string InsightsApiKey = "23a3e3502a5258fbc6bf97bd85341af8b4a0b69b";
 		public static readonly string FlikrApiKey = "22a18bbd0b4f7c86975c81c8f3cb7719";
 
+
 		public static readonly string GoogleApiClientId = "236481934978-balamfh77inmje2nfeq3bphdg3udt03t.apps.googleusercontent.com";
 		public static readonly string GoogleClientSecret = "wRYG9TlQZLXnlpUbu-0X_BwF";
 		public static readonly string GoogleOAuthScope = "profile email https://www.googleapis.com/auth/calendar";
