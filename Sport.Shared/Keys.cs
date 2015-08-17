4f2829ad6542795f861a08302449bb3395ee9c46
rob-derosa <rob.derosa@xamarin.com>
2015-08-16 17:49:35 -0700

    Minor updates

--- Sport.Shared/Keys.cs
+++ Sport.Shared/Keys.cs
@@ -2,18 +2,17 @@
 {
 	public class Keys
 	{
-		public static readonly string AzureDomain = "";
+		public static readonly string AzureDomain = "http://...";
+		public static readonly string AzureApplicationKey = "";
+
 		public static readonly string InsightsApiKey = "";
-		public static readonly string SourceCodeUrl = "";
-
-		public static readonly string GoogleApiClientId = "";
-		public static readonly string GoogleClientSecret = "";
-		public static readonly string GoogleOAuthScope = "profile email";
-		public static readonly string GoogleOAthRedirectUrl = "http://localhost";
-		public static readonly string GoogleOAuthAuthUrl = "";
-		public static readonly string GoogleOAuthTokenUrl = "";
-
-		public static readonly string AzureClientId = "";
 		public static readonly string FlikrApiKey = "";
+		public static readonly string SourceCodeUrl = "https://github.com/...";
+
+		public static readonly string GoogleApiClientId = "...apps.googleusercontent.com";
+		public static readonly string GoogleClientSecret = "";
+		public static readonly string GoogleOAuthScope = "profile email https://www.googleapis.com/auth/calendar";
+		public static readonly string GoogleOAthRedirectUrl = "http://localhost";
+		public static readonly string GooglePushNotificationSenderId = "236481934978";
 	}
 }
